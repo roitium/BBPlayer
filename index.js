3ac1a372b75317533348576acc830aeae4932201
roitium <65794453+yanyao2333@users.noreply.github.com>
2025-03-09 12:27:17 +0800

    fix: typo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native'
 import 'expo-router/entry'
 import { name as appName } from './app.json'
 import { PlaybackService } from './lib/services/PlaybackService'
+import TrackPlayer from 'react-native-track-player'
 
 AppRegistry.registerComponent(appName, () => App)
 
