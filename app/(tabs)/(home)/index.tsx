17fceb3905549b5a311cf1b65282599cebce4caf
roitium <65794453+yanyao2333@users.noreply.github.com>
2025-03-14 22:41:56 +0800

    chore: remove useless codes

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -5,7 +5,6 @@ import {
   Image,
   TouchableOpacity,
   RefreshControl,
-  Alert,
 } from 'react-native'
 import {
   Text,
@@ -20,7 +19,7 @@ import {
   Button,
 } from 'react-native-paper'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import {
   usePopularVideos,
   useRecentlyPlayed,
@@ -30,7 +29,6 @@ import type { Track, Playlist } from '@/types/core/media'
 import { usePlayerStore } from '@/lib/store/usePlayerStore'
 import useAppStore from '@/lib/store/useAppStore'
 import { formatDurationToHHMM } from '@/utils/times'
-import Toast from 'react-native-toast-message'
 
 const mockCategories = [
   { id: '1', name: '翻唱', icon: 'music-note' },
@@ -151,20 +149,6 @@ const HomePage = () => {
     setRefreshing(false)
   }
 
-  useEffect(() => {
-    if (recentlyPlayed?.[0]?.cover) {
-      console.log('测试图片：', recentlyPlayed?.[0]?.cover)
-      fetch(recentlyPlayed?.[0]?.cover).then((res) => {
-        console.log('测试图片：', res)
-        Toast.show({
-          text1: `测试图片：${recentlyPlayed?.[0]?.cover}`,
-          text2: `状态：${res.status}`,
-          type: 'success',
-        })
-      })
-    }
-  }, [recentlyPlayed])
-
   // 渲染最近播放项
   const renderRecentItem = (item: Track) => (
     <TouchableOpacity
@@ -182,15 +166,15 @@ const HomePage = () => {
             source={{ uri: item.cover }}
             className='rounded'
             style={{ width: 48, height: 48 }}
-            onError={(error) => {
-              // console.log('图片加载失败：', error)
-              // Toast.show({
-              //   text1: `图片加载失败：${item.cover}`,
-              //   text2: `错误：${error.nativeEvent.error}`,
-              //   type: 'error',
-              // })
-              Alert.alert('图片加载失败', error.nativeEvent.error)
-            }}
+            // onError={(error) => {
+            //   // console.log('图片加载失败：', error)
+            //   // Toast.show({
+            //   //   text1: `图片加载失败：${item.cover}`,
+            //   //   text2: `错误：${error.nativeEvent.error}`,
+            //   //   type: 'error',
+            //   // })
+            //   Alert.alert('图片加载失败', error.nativeEvent.error)
+            // }}
           />
           <View className='ml-3 flex-1'>
             <Text
