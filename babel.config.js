8dfb1c66a913faaff03e62138baa050cb11a24b5
roitium <65794453+yanyao2333@users.noreply.github.com>
2025-03-27 20:04:12 +0800

    fix: remove console.log when build production version

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = (api) => {
-	api.cache(true);
-	return {
-		presets: [
-			["babel-preset-expo", { jsxImportSource: "nativewind" }],
-			"nativewind/babel",
-		],
-		env: {
-			production: {
-				plugins: ["react-native-paper/babel"],
-			},
-		},
-	};
-};
+  api.cache(true)
+  return {
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel', 'transform-remove-console'],
+      },
+    },
+  }
+}
