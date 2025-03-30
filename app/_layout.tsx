2cff7af8e18ed1a22eb7f6db0777383e6dabad37
roitium <65794453+yanyao2333@users.noreply.github.com>
2025-03-30 21:27:26 +0800

    fix: edit sentry config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,6 +33,13 @@ import { BilibiliApiError } from '@/utils/errors'
 import GlobalErrorFallback from '@/components/ErrorBoundary'
 import { setupPlayer } from '@/lib/services/setupPlayer'
 import { showToast } from '@/utils/toast'
+import * as Updates from 'expo-updates'
+
+const manifest = Updates.manifest
+const metadata = 'metadata' in manifest ? manifest.metadata : undefined
+const extra = 'extra' in manifest ? manifest.extra : undefined
+const updateGroup =
+  metadata && 'updateGroup' in metadata ? metadata.updateGroup : undefined
 
 const developement = process.env.NODE_ENV === 'development'
 
@@ -51,10 +58,30 @@ const navigationIntegration = Sentry.reactNavigationIntegration({
 Sentry.init({
   dsn: 'https://893ea8eb3743da1e065f56b3aa5e96f9@o4508985265618944.ingest.us.sentry.io/4508985267191808',
   debug: false,
-  tracesSampleRate: developement ? 1 : 0.8,
-  integrations: [navigationIntegration],
+  tracesSampleRate: developement ? 1 : 0.7,
+  sendDefaultPii: true,
+  integrations: [navigationIntegration, Sentry.mobileReplayIntegration()],
   enableNativeFramesTracking: !isRunningInExpoGo(),
+  profilesSampleRate: developement ? 0 : 0.1,
 })
+
+const scope = Sentry.getGlobalScope()
+
+scope.setTag('expo-update-id', Updates.updateId)
+scope.setTag('expo-is-embedded-update', Updates.isEmbeddedLaunch)
+
+if (typeof updateGroup === 'string') {
+  scope.setTag('expo-update-group-id', updateGroup)
+
+  const owner = extra?.expoClient?.owner ?? '[account]'
+  const slug = extra?.expoClient?.slug ?? '[project]'
+  scope.setTag(
+    'expo-update-debug-url',
+    `https://expo.dev/accounts/${owner}/projects/${slug}/updates/${updateGroup}`,
+  )
+} else if (Updates.isEmbeddedLaunch) {
+  scope.setTag('expo-update-debug-url', 'not applicable for embedded updates')
+}
 
 // 设置全局错误处理器，捕获未被处理的 JS 错误
 if (!developement) {
