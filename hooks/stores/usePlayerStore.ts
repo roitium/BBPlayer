fa68bd6b1bd7953f5017939b4bf00beace4872fe
Roitium <65794453+yanyao2333@users.noreply.github.com>
2025-06-07 18:15:43 +0800

    chore: remove some log

--- hooks/stores/usePlayerStore.ts
+++ hooks/stores/usePlayerStore.ts
@@ -40,9 +40,6 @@ const checkPlayerReady = () => {
  * 采用 zustand 自己维护一个 queue，rntp 仅用于播放当前的 track，通过 TrackPlayer.load 来替换当前播放的内容，所有队列操作都通过该 store 进行
  */
 export const usePlayerStore = create<PlayerStore>()((set, get) => {
-	playerLog.debug('创建播放器状态存储')
-
-	// 初始状态
 	const initialState: PlayerState = {
 		queue: [],
 		currentIndex: -1,
@@ -54,16 +51,9 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 		shuffledQueue: [],
 	}
 
-	playerLog.debug('初始化播放器状态', initialState)
-
-	// 创建store
 	const store = {
 		...initialState,
 
-		/**
-		 * 重置播放器
-		 * 在应用退出或需要重置播放器时调用
-		 */
 		resetPlayer: async () => {
 			if (!global.playerIsReady) return
 
@@ -75,19 +65,16 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 			}
 		},
 
-		// rntpQueue (现在始终返回一个只有一个元素的数组)
 		rntpQueue: async () => {
-			playerLog.debug('调用 rntpQueue()')
 			const currentTrack = await TrackPlayer.getActiveTrack()
 			return currentTrack ? [currentTrack] : []
 		},
 
 		removeTrack: async (id: string, cid: number | undefined) => {
-			playerLog.debug('调用 removeTrack()', { id, cid })
+			playerLog.debug('removeTrack()', { id, cid })
 			const { queue, shuffledQueue, shuffleMode, currentTrack } = get()
 			const currentQueue = shuffleMode ? shuffledQueue : queue
 			if (currentTrack?.id === id && currentTrack?.cid === cid) {
-				playerLog.debug('当前正在播放的曲目被删除，判断是否是最后一首')
 				if (currentQueue.length === 1) {
 					playerLog.debug('队列只有一首歌曲，清空队列并停止播放')
 					set(
@@ -106,19 +93,14 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				const lastTrack = currentQueue.at(-1)
 				if (lastTrack) {
 					if (lastTrack.id === id && lastTrack.cid === cid) {
-						playerLog.debug('是最后一首，则跳转到上一首')
 						await get().skipToPrevious()
 					} else {
-						playerLog.debug('不是最后一首，则跳转到下一首')
 						await get().skipToNext()
 					}
 				}
 			}
 			const queueIndex = queue.findIndex((t) => t.id === id && t.cid === cid)
 			if (queueIndex === -1) {
-				playerLog.debug(
-					'在队列中找不到该曲目，无法删除，已记录日志并重置播放器',
-				)
 				Toast.error('播放器异常', {
 					description: '在播放列表中找不到该曲目，已重置播放器',
 				})
@@ -130,9 +112,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 			)
 			// 当没有开启过随机模式时，shuffledQueue 为空，所以在判断 shuffledQueueIndex 时还需要判断列表不为空
 			if (shuffledQueueIndex === -1 && shuffledQueue.length > 0) {
-				playerLog.debug(
-					'在随机队列中找不到该曲目，无法删除，已上报日志并重置播放器',
-				)
 				Toast.error('播放器异常', {
 					description: '在播放列表中找不到该曲目，已重置播放器',
 				})
@@ -166,26 +145,19 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 			startFromCid,
 			playNext,
 		}: addToQueueParams) => {
-			playerLog.debug('调用 addToQueue()', {
+			playerLog.debug('addToQueue()', {
 				tracksCount: tracks.length,
-				tracks: tracks.map((t) => ({ id: t.id, title: t.title })),
 				playNow,
-				startFromId,
 				clearQueue,
+				playNext,
 			})
 
 			if (!checkPlayerReady()) return
-
-			if (tracks.length === 0) {
-				playerLog.debug('无曲目需要添加')
-				return
-			}
-
+			if (tracks.length === 0) return
 			if (playNow && playNext) {
 				playerLog.error('playNow 和 playNext 不能同时为 true')
 				return
 			}
-
 			if (startFromCid && startFromId) {
 				playerLog.error('startFromCid 和 startFromId 不能同时填写')
 				return
@@ -201,6 +173,7 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 			)
 
 			if (newTracks.length === 0) {
+				playerLog.debug('无新曲目需要添加')
 				const switchTrackPipe = async (targetIndex: number) => {
 					set(
 						produce((state: PlayerState) => {
@@ -216,7 +189,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 					set({ isPlaying: true })
 				}
 
-				playerLog.debug('无新曲目需要添加')
 				if (playNow && (startFromId || startFromCid)) {
 					if (get().shuffleMode) {
 						const targetIndex = get().shuffledQueue.findIndex((t) =>
@@ -238,9 +210,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 					const targetIndex = get().queue.findIndex((t) =>
 						isTargetTrack(t, tracks[0].id, tracks[0].cid),
 					)
-					playerLog.debug('没有指定播放新曲目，则播放新队列的第一首', {
-						targetIndex,
-					})
 					if (targetIndex !== -1) {
 						await switchTrackPipe(targetIndex)
 					}
@@ -250,12 +219,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 
 			set(
 				produce((state: PlayerState) => {
-					playerLog.debug('当前队列状态', {
-						queueLength: state.queue.length,
-						currentIndex: state.currentIndex,
-						currentTrack: state.currentTrack?.title,
-					})
-
 					let insertIndexForQueue = state.queue.length // 默认为队列末尾
 					if (playNext && state.currentIndex !== -1) {
 						insertIndexForQueue = state.currentIndex + 1
@@ -336,16 +299,12 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				preloadEndIndex,
 			)
 
-			playerLog.debug('预加载曲目', {
-				preloadStartIndex,
-				preloadEndIndex,
-				tracksToPreload: tracksToPreload.map((t) => t.title),
-			})
-
-			await Promise.all(
-				tracksToPreload.map((track) => get().patchMetadataAndAudio(track)),
-			)
-			playerLog.debug('预加载完成')
+			if (tracksToPreload.length > 0) {
+				playerLog.debug(`开始预加载 ${tracksToPreload.length} 首曲目`)
+				await Promise.all(
+					tracksToPreload.map((track) => get().patchMetadataAndAudio(track)),
+				)
+			}
 		},
 
 		// 切换播放/暂停
@@ -359,55 +318,42 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				patchMetadataAndAudio,
 				seekTo,
 			} = get()
-			playerLog.debug('调用 togglePlay()', {
-				isPlaying,
-				currentTrack: currentTrack?.title,
-				currentIndex: currentIndex,
-			})
 
 			if (!checkPlayerReady()) return
-
-			if (!currentTrack) {
-				playerLog.debug('当前没有播放的曲目，无需操作')
-				return
-			}
+			if (!currentTrack) return
 
 			try {
 				if (!(await rntpQueue()).length) {
-					playerLog.debug('队列为空，如果当前有曲目，尝试重新加载')
+					playerLog.debug('rntp 队列为空, 尝试重新加载当前曲目')
 					if (currentTrack) {
 						skipToTrack(currentIndex)
 					}
+					return // skipToTrack will handle playback
 				}
+
 				if (isPlaying) {
-					playerLog.debug('当前正在播放，执行暂停')
 					await TrackPlayer.pause()
 				} else {
-					playerLog.debug(
-						'当前已暂停，即将播放，让我们做些检查看看 track 是否过期',
-					)
 					const isExpired = checkBilibiliAudioExpiry(currentTrack)
 					if (!isExpired) {
-						playerLog.debug('音频流没过期，继续播放')
 						await TrackPlayer.play()
 						set({ isPlaying: true })
 						return
 					}
+
+					playerLog.debug('音频流已过期, 正在更新...')
 					const result = await patchMetadataAndAudio(currentTrack)
 					if (result.isErr()) {
 						playerLog.sentry('更新音频流失败', result.error)
 						return
 					}
+
 					const { needsUpdate, track } = result.value
 					if (needsUpdate) {
-						// 如果需要更新音频流，则先替换掉当前播放的歌曲，并将播放位置恢复到上次播放的位置
-						playerLog.debug(
-							'音频流需要更新，刚刚更新过了，现在替换掉当前播放的歌曲',
-						)
 						const { position } = await TrackPlayer.getProgress()
 						const rntpTrack = convertToRNTPTrack(track)
 						if (rntpTrack.isErr()) {
-							playerLog.sentry('更新音频流失败', rntpTrack.error)
+							playerLog.sentry('转换为 RNTPTrack 失败', rntpTrack.error)
 							return
 						}
 						await TrackPlayer.load(rntpTrack.value)
@@ -415,25 +361,15 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 					}
 					await TrackPlayer.play()
 				}
-
 				set({ isPlaying: !isPlaying })
-				playerLog.debug('状态已更新：isPlaying =', !isPlaying)
 			} catch (error) {
 				playerLog.sentry('切换播放状态失败', error)
 			}
 		},
 
-		// 下一曲
 		skipToNext: async () => {
 			const { queue, currentIndex, shuffleMode, repeatMode, shuffledQueue } =
 				get()
-			playerLog.debug('调用 skipToNext()', {
-				queueLength: queue.length,
-				currentIndex,
-				shuffleMode,
-				repeatMode,
-				currentTrack: get().currentTrack?.title,
-			})
 
 			if (!checkPlayerReady()) return
 
@@ -441,43 +377,32 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				const currentQueue = shuffleMode ? shuffledQueue : queue
 
 				if (currentQueue.length <= 1) {
-					playerLog.debug('队列中没有（或只有一首）曲目，无法跳转')
-					await TrackPlayer.pause() // 停止播放
+					playerLog.debug('队列中曲目不足, 无法跳转下一首')
+					await TrackPlayer.pause()
 					set({ isPlaying: false })
 					return
 				}
 
 				let nextIndex: number
-
 				if (repeatMode === RepeatMode.Queue) {
-					// 列表循环
 					nextIndex = (currentIndex + 1) % currentQueue.length
 				} else {
-					// 顺序播放 & 关闭循环
 					nextIndex = currentIndex + 1
 					if (nextIndex >= currentQueue.length) {
-						playerLog.debug('已到达最后一首，停止播放')
-						await TrackPlayer.pause() // 停止播放
+						playerLog.debug('已到达最后一首, 停止播放')
+						await TrackPlayer.pause()
 						set({ isPlaying: false })
 						return
 					}
 				}
-
-				await get().skipToTrack(nextIndex) // 切换到下一首
+				await get().skipToTrack(nextIndex)
 			} catch (error) {
 				playerLog.sentry('跳转到下一曲失败', error)
 			}
 		},
 
-		// 上一曲
 		skipToPrevious: async () => {
 			const { queue, currentIndex, shuffleMode, shuffledQueue } = get()
-			playerLog.debug('调用 skipToPrevious()', {
-				queueLength: queue.length,
-				currentIndex,
-				shuffleMode,
-				currentTrack: get().currentTrack?.title,
-			})
 
 			if (!checkPlayerReady()) return
 
@@ -485,7 +410,7 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				const currentQueue = shuffleMode ? shuffledQueue : queue
 
 				if (currentQueue.length <= 1) {
-					playerLog.debug('队列中没有（或只有一首）曲目，无法跳转')
+					playerLog.debug('队列中曲目不足, 无法跳转上一首')
 					return
 				}
 
@@ -499,31 +424,18 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 			}
 		},
 
-		// 跳转到指定位置
 		seekTo: async (position: number) => {
-			playerLog.debug('调用 seekTo()', {
-				position,
-				currentTrack: get().currentTrack?.title,
-				currentIndex: get().currentIndex,
-			})
-
 			if (!checkPlayerReady()) return
 
 			try {
 				await TrackPlayer.seekTo(position)
-				playerLog.debug('跳转成功', { position })
 			} catch (error) {
 				playerLog.sentry('跳转到指定位置失败', error)
 			}
 		},
 
-		// 切换重复模式
 		toggleRepeatMode: async () => {
 			const { repeatMode } = get()
-			playerLog.debug('调用 toggleRepeatMode()', {
-				currentMode: repeatMode,
-			})
-
 			if (!checkPlayerReady()) return
 
 			let newMode: RepeatMode
@@ -539,25 +451,18 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				await TrackPlayer.setRepeatMode(RepeatMode.Off)
 			}
 			set({ repeatMode: newMode })
-			playerLog.debug('状态已更新：重复模式已更改', { newMode })
+			playerLog.debug('重复模式已更改', { newMode })
 		},
 
-		// 切换随机模式
 		toggleShuffleMode: () => {
 			const { shuffleMode, queue, currentIndex } = get()
-			playerLog.debug('调用 toggleShuffleMode()', {
-				currentMode: shuffleMode,
-			})
-
 			if (!checkPlayerReady()) return
 
 			if (shuffleMode) {
-				// 关闭随机模式，恢复原始队列
-				playerLog.debug('关闭随机模式，恢复原始队列')
+				playerLog.debug('关闭随机模式')
 				set(
 					produce((state: PlayerState) => {
 						state.shuffleMode = false
-						// 找到当前播放曲目在原始队列中的索引
 						if (state.currentTrack) {
 							const currentTrack = state.currentTrack
 							state.currentIndex = state.queue.findIndex((track) =>
@@ -568,11 +473,8 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 					}),
 				)
 			} else {
-				// 开启随机模式，打乱队列
-				playerLog.debug('开启随机模式，打乱队列')
-
-				const shuffledQueue = [...queue] // 创建队列的副本
-				// Fisher-Yates 洗牌算法(网上抄的)
+				playerLog.debug('开启随机模式')
+				const shuffledQueue = [...queue]
 				for (let i = shuffledQueue.length - 1; i > 0; i--) {
 					const j = Math.floor(Math.random() * (i + 1))
 					;[shuffledQueue[i], shuffledQueue[j]] = [
@@ -581,7 +483,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 					]
 				}
 
-				// 确保当前播放的歌曲在打乱后的队列中位置不变
 				const currentTrackIndex = shuffledQueue.findIndex((track) =>
 					isTargetTrack(track, queue[currentIndex].id, queue[currentIndex].cid),
 				)
@@ -596,25 +497,18 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 					produce((state: PlayerState) => {
 						state.shuffleMode = true
 						state.shuffledQueue = shuffledQueue
-						state.currentIndex = 0 // 重置索引为0，因为当前播放的歌曲现在是打乱后队列的第一首
+						state.currentIndex = 0
 					}),
 				)
 			}
 		},
 
-		// 清空队列
 		clearQueue: async () => {
-			playerLog.debug('调用 clearQueue()', {
-				currentQueueLength: get().queue.length,
-				currentTrack: get().currentTrack?.title,
-			})
-
+			playerLog.debug('清空队列')
 			if (!checkPlayerReady()) return
 
 			try {
-				playerLog.debug('重置播放器')
 				await TrackPlayer.reset()
-
 				set(
 					produce((store) => {
 						store.queue = []
@@ -625,33 +519,23 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 						store.isBuffering = false
 					}),
 				)
-				playerLog.debug('状态已更新：队列已清空，播放器已重置')
 			} catch (error) {
 				playerLog.sentry('清空队列失败', error)
 			}
 		},
 
-		// 检查并更新音频流和元数据，并将最终结果更新到 queue 中
 		patchMetadataAndAudio: async (
 			track: Track,
 		): Promise<
 			Result<{ track: Track; needsUpdate: boolean }, BilibiliApiError | unknown>
 		> => {
-			playerLog.debug('调用 patchMetadataAndAudio()', {
-				trackId: track.id,
-				title: track.title,
-				source: track.source,
-			})
 			let updatedTrack = track
 			try {
 				if (!track.hasMetadata) {
-					playerLog.debug('这个 track 没有元数据，先获取元数据', {
-						trackId: track.id,
-						cid: track.cid,
-					})
+					playerLog.debug(`获取元数据: id=${track.id}, cid=${track.cid}`)
 					const metadata = await bilibiliApi.getVideoDetails(track.id)
 					if (metadata.isErr()) {
-						playerLog.sentry('获取元数据失败,返回原始track', metadata.error)
+						playerLog.sentry('获取元数据失败', metadata.error)
 						return err(metadata.error)
 					}
 					updatedTrack = {
@@ -665,7 +549,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 						hasMetadata: true,
 					}
 
-					// 在这里立即更新当前曲目的信息，避免显示空白。
 					set(
 						produce((state: PlayerState) => {
 							const queueIndex = state.queue.findIndex((t) =>
@@ -682,14 +565,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 							}
 						}),
 					)
-					playerLog.debug('B站音频元数据获取成功，开始继续检查音频流', {
-						trackId: updatedTrack.id,
-						title: updatedTrack.title,
-						artist: updatedTrack.artist,
-						cover: updatedTrack.cover,
-						duration: updatedTrack.duration,
-						createTime: updatedTrack.createTime,
-					})
 				}
 				const result = await checkAndUpdateAudioStream(updatedTrack)
 				if (result.isErr()) {
@@ -697,10 +572,6 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				}
 				const { track: finalTrack, needsUpdate } = result.value
 				if (needsUpdate) {
-					playerLog.debug('音频流需要更新，更新到 queue 中', {
-						trackId: finalTrack.id,
-						title: finalTrack.title,
-					})
 					set(
 						produce((state: PlayerState) => {
 							const queueIndex = state.queue.findIndex((t) =>
@@ -724,23 +595,16 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 			}
 		},
 
-		// 跳转到指定曲目
 		skipToTrack: async (index: number) => {
-			const { queue, shuffleMode, shuffledQueue } = get()
+			const { shuffleMode, shuffledQueue, queue } = get()
 			const currentQueue = shuffleMode ? shuffledQueue : queue
 
-			playerLog.debug('调用 skipToTrack()', {
-				index,
-				queueLength: queue.length,
-				shuffledQueueLength: shuffledQueue.length,
-				shuffleMode,
-				currentTrack: get().currentTrack?.title,
-			})
-
 			if (!checkPlayerReady()) return
-
 			if (index < 0 || index >= currentQueue.length) {
-				playerLog.debug('索引超出范围', { index })
+				playerLog.debug('skipToTrack 索引超出范围', {
+					index,
+					queueSize: currentQueue.length,
+				})
 				return
 			}
 
@@ -749,63 +613,51 @@ export const usePlayerStore = create<PlayerStore>()((set, get) => {
 				playerLog.debug('未找到指定索引的曲目', { index })
 				return
 			}
-			// 乐观更新当前曲目，让界面显示更迅速
+
+			playerLog.debug(`跳转到曲目: index=${index}, title=${track.title}`)
 			if (track.hasMetadata) {
 				set({ currentTrack: track, isBuffering: true, currentIndex: index })
 			}
 
-			// 检查并更新音频流
-			const updatedTrack = await get().patchMetadataAndAudio(track)
-			if (updatedTrack.isErr()) {
-				playerLog.sentry('更新音频流失败', updatedTrack.error)
+			const updatedTrackResult = await get().patchMetadataAndAudio(track)
+			if (updatedTrackResult.isErr()) {
+				playerLog.sentry('更新音频流失败', updatedTrackResult.error)
 				TrackPlayer.pause()
 				Toast.error('播放失败: 更新音频流失败', {
-					description: `id: ${track.id}，错误：${updatedTrack.error}`,
+					description: `id: ${track.id}，错误：${updatedTrackResult.error}`,
 					duration: Number.POSITIVE_INFINITY,
 				})
 				return
 			}
+			const updatedTrack = updatedTrackResult.value.track
 
-			// 使用 TrackPlayer.load() 替换当前曲目
-			const rntpTrack = convertToRNTPTrack(updatedTrack.value.track)
-			if (rntpTrack.isErr()) {
-				playerLog.sentry('更新音频流失败', rntpTrack.error)
+			const rntpTrackResult = convertToRNTPTrack(updatedTrack)
+			if (rntpTrackResult.isErr()) {
+				playerLog.sentry('转换为 RNTP 对象失败', rntpTrackResult.error)
 				TrackPlayer.pause()
-				Toast.error('播放失败: 转换为 RNTP 对象失败', {
-					description: `id: ${updatedTrack.value.track.id}，错误：${rntpTrack.error}`,
+				Toast.error('播放失败: 转换播放器对象失败', {
+					description: `id: ${updatedTrack.id}，错误：${rntpTrackResult.error}`,
 					duration: Number.POSITIVE_INFINITY,
 				})
 				return
 			}
-			await TrackPlayer.load(rntpTrack.value)
-			reportPlaybackHistory(updatedTrack.value.track).catch((error) =>
-				playerLog.error(
-					'wtf??? 捕获到了 reportPlaybackHistory 的错误，但这本不应该发生',
-					error,
-				),
+			await TrackPlayer.load(rntpTrackResult.value)
+			reportPlaybackHistory(updatedTrack).catch((error) =>
+				playerLog.error('上报播放历史失败 (捕获到非预期错误)', error),
 			)
 
-			// 更新状态 (在 load 之后，确保状态是最新的）
 			set(
 				produce((state: PlayerState) => {
 					state.currentIndex = index
-					state.currentTrack = updatedTrack.value.track
+					state.currentTrack = updatedTrack
 				}),
 			)
-
-			// 预加载
 			get().preloadTracks(index)
-
-			playerLog.debug('已跳转到指定曲目', {
-				index,
-				trackTitle: updatedTrack.value.track.title,
-			})
 		},
 	}
 
 	return store
 })
 
-// 导出一些有用的hooks
 export const usePlaybackProgress = useProgress
 export const usePlaybackStateHook = usePlaybackState
