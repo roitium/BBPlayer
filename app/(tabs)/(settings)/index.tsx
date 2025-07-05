c60a54b3529a7d83df0a67c549846530d2f380c9
Roitium <65794453+yanyao2333@users.noreply.github.com>
2025-07-05 22:36:30 +0800

    fix: show hotfix created time

--- app/(tabs)/(settings)/index.tsx
+++ app/(tabs)/(settings)/index.tsx
@@ -12,11 +12,14 @@ import useAppStore from '@/hooks/stores/useAppStore'
 import type { RootStackParamList } from '../../../types/navigation'
 import * as Updates from 'expo-updates'
 
-const CLICK_TIMES = 3;
+const CLICK_TIMES = 3
+const updateTime = Updates.createdAt
+	? `${Updates.createdAt.getFullYear()}-${Updates.createdAt.getMonth() + 1}-${Updates.createdAt.getDate()}`
+	: ''
 
 export default function SettingsPage() {
-	const insets = useSafeAreaInsets();
-	const colors = useTheme().colors;
+	const insets = useSafeAreaInsets()
+	const colors = useTheme().colors
 	return (
 		<View
 			style={{
@@ -29,12 +32,15 @@ export default function SettingsPage() {
 				style={{
 					paddingHorizontal: 25,
 					paddingBottom: 20,
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-between',
 				}}
 			>
-				<Text variant="headlineSmall" style={{ fontWeight: "bold" }}>
+				<Text
+					variant='headlineSmall'
+					style={{ fontWeight: 'bold' }}
+				>
 					设置
 				</Text>
 			</View>
@@ -45,120 +51,133 @@ export default function SettingsPage() {
 				contentContainerStyle={{
 					paddingHorizontal: 25,
 				}}
-				contentInsetAdjustmentBehavior="automatic"
+				contentInsetAdjustmentBehavior='automatic'
 			>
 				<SettingsSection />
 			</ScrollView>
 			<Divider style={{ marginTop: 16, marginBottom: 16 }} />
 			<AboutSection />
 		</View>
-	);
+	)
 }
 
 const AboutSection = memo(function AboutSection() {
 	const navigation =
-		useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-	const [clickTimes, setClickTimes] = useState(0);
+		useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+	const [clickTimes, setClickTimes] = useState(0)
 
 	const handlePress = useCallback(() => {
-		setClickTimes(clickTimes + 1);
+		setClickTimes(clickTimes + 1)
 		if (clickTimes >= CLICK_TIMES) {
-			navigation.navigate("Test");
+			navigation.navigate('Test')
 			setTimeout(() => {
-				setClickTimes(0);
-			}, 200);
-			return;
+				setClickTimes(0)
+			}, 200)
+			return
 		}
-	}, [clickTimes, navigation]);
+	}, [clickTimes, navigation])
 
 	return (
 		<View style={{ paddingBottom: 15 }}>
 			<Text
-				variant="titleLarge"
-				style={{ textAlign: "center", marginBottom: 5 }}
+				variant='titleLarge'
+				style={{ textAlign: 'center', marginBottom: 5 }}
 				onPress={handlePress}
 			>
 				BBPlayer
 			</Text>
 			<Text
-				variant='bodyMedium'
+				variant='bodySmall'
 				style={{ textAlign: 'center', marginBottom: 5 }}
 			>
 				v{Application.nativeApplicationVersion}:{Application.nativeBuildVersion}{' '}
-				{Updates.updateId ? `(hotfix-${Updates.updateId.slice(0, 7)})` : ''}
+				{Updates.updateId
+					? `(hotfix-${Updates.updateId.slice(0, 7)}-${updateTime})`
+					: ''}
 			</Text>
-			<Text variant="bodyMedium" style={{ textAlign: "center" }}>
-				一个<Text style={{ textDecorationLine: "line-through" }}>简陋</Text>的
+
+			<Text
+				variant='bodyMedium'
+				style={{ textAlign: 'center' }}
+			>
+				一个<Text style={{ textDecorationLine: 'line-through' }}>简陋</Text>的
 				Bilibili 音乐播放器
 			</Text>
-			<Text variant="bodyMedium" style={{ textAlign: "center", marginTop: 8 }}>
+			<Text
+				variant='bodyMedium'
+				style={{ textAlign: 'center', marginTop: 8 }}
+			>
 				开源地址：
 				<Text
-					variant="bodyMedium"
+					variant='bodyMedium'
 					onPress={() =>
 						WebBrowser.openBrowserAsync(
-							"https://github.com/yanyao2333/BBPlayer",
+							'https://github.com/yanyao2333/BBPlayer',
 						)
 					}
-					style={{ textDecorationLine: "underline" }}
+					style={{ textDecorationLine: 'underline' }}
 				>
 					https://github.com/roitium/BBPlayer
 				</Text>
 			</Text>
 		</View>
-	);
-});
+	)
+})
 
-AboutSection.displayName = "AboutSection";
+AboutSection.displayName = 'AboutSection'
 
 const SettingsSection = memo(function SettingsSection() {
 	const setSendPlayHistory = useAppStore(
 		(state) => state.setEnableSendPlayHistory,
-	);
-	const sendPlayHistory = useAppStore(
-		(state) => state.settings.sendPlayHistory,
-	);
-	const [cookieDialogVisible, setCookieDialogVisible] = useState(false);
+	)
+	const sendPlayHistory = useAppStore((state) => state.settings.sendPlayHistory)
+	const [cookieDialogVisible, setCookieDialogVisible] = useState(false)
 	const [isQrCodeLoginDialogVisible, setIsQrCodeLoginDialogVisible] =
-		useState(false);
+		useState(false)
 
 	return (
-		<View style={{ flexDirection: "column" }}>
+		<View style={{ flexDirection: 'column' }}>
 			<View
 				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-between',
 					marginTop: 16,
 				}}
 			>
 				<Text>向 bilibili 上报观看进度</Text>
-				<Switch value={sendPlayHistory} onValueChange={setSendPlayHistory} />
+				<Switch
+					value={sendPlayHistory}
+					onValueChange={setSendPlayHistory}
+				/>
 			</View>
 			<View
 				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-between',
 					marginTop: 16,
 				}}
 			>
 				<Text>手动设置 Cookie</Text>
-				<Button mode="contained" onPress={() => setCookieDialogVisible(true)}>
+				<Button
+					mode='contained'
+					onPress={() => setCookieDialogVisible(true)}
+				>
 					打开窗口
 				</Button>
 			</View>
 			<View
 				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-between',
 					marginTop: 16,
 				}}
 			>
 				<Text>重新扫码登录</Text>
 				<Button
-					mode="contained"
+					mode='contained'
 					onPress={() => setIsQrCodeLoginDialogVisible(true)}
 				>
 					打开窗口
@@ -174,7 +193,7 @@ const SettingsSection = memo(function SettingsSection() {
 				setVisible={setIsQrCodeLoginDialogVisible}
 			/>
 		</View>
-	);
-});
+	)
+})
 
-SettingsSection.displayName = "SettingsSection";
+SettingsSection.displayName = 'SettingsSection'
