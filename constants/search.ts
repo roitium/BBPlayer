fdf4c5de2898c874857be56ef314479b620c67a3
Roitium <65794453+yanyao2333@users.noreply.github.com>
2025-07-12 22:10:26 +0800

    chore: add more multipage video keywords

--- constants/search.ts
+++ constants/search.ts
@@ -1,3 +1,12 @@
-const MULTIPAGE_VIDEO_KEYWORDS = ['分P系列', '分p系列', '分P', '分p', 'OST']
+const MULTIPAGE_VIDEO_KEYWORDS = [
+	'分P系列',
+	'分p系列',
+	'分P',
+	'分p',
+	'OST',
+	'原声带',
+	'专辑',
+	'Original Soundtrack',
+]
 
 export { MULTIPAGE_VIDEO_KEYWORDS }
