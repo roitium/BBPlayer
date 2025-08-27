0d50fa7ace07d0642de7ed227bf25c07cf630cc2
Roitium <65794453+yanyao2333@users.noreply.github.com>
2025-08-27 12:30:21 +0800

    feat: alert when replace playlist

--- app/playlist/local/hooks/useLocalPlaylistPlayer.ts
+++ app/playlist/local/hooks/useLocalPlaylistPlayer.ts
@@ -1,12 +1,17 @@
+import { alert } from '@/components/modals/AlertModal'
 import { usePlayerStore } from '@/hooks/stores/usePlayerStore'
 import type { Track } from '@/types/core/media'
 import { toastAndLogError } from '@/utils/log'
+import { storage } from '@/utils/mmkv'
 import { useCallback } from 'react'
+import { useMMKVBoolean } from 'react-native-mmkv'
 
 const SCOPE = 'UI.Playlist.Local.Player'
 
 export function useLocalPlaylistPlayer(tracks: Track[]) {
 	const addToQueue = usePlayerStore((state) => state.addToQueue)
+	const [ignoreAlertReplacePlaylist, setIgnoreAlertReplacePlaylist] =
+		useMMKVBoolean('ignore_alert_replace_playlist', storage)
 
 	const playAll = useCallback(
 		async (startFromId?: string) => {
@@ -28,9 +33,27 @@ export function useLocalPlaylistPlayer(tracks: Track[]) {
 
 	const handleTrackPress = useCallback(
 		(track: Track) => {
+			if (!ignoreAlertReplacePlaylist) {
+				alert(
+					'替换播放列表',
+					'点击列表中的单曲会直接替换当前播放列表，是否继续？（下次不再提醒）',
+					[
+						{ text: '取消' },
+						{
+							text: '确定',
+							onPress: () => {
+								setIgnoreAlertReplacePlaylist(true)
+								void playAll(track.uniqueKey)
+							},
+						},
+					],
+					{ cancelable: true },
+				)
+				return
+			}
 			void playAll(track.uniqueKey)
 		},
-		[playAll],
+		[ignoreAlertReplacePlaylist, playAll, setIgnoreAlertReplacePlaylist],
 	)
 
 	return { playAll, handleTrackPress }
