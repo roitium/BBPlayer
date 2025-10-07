59bd442fbf9122e513c4b08f819faa4745d4ab35
Roitium <65794453+roitium@users.noreply.github.com>
2025-10-07 23:08:08 +0800

    fix: 1

--- utils/__mocks__/log.ts
+++ utils/__mocks__/log.ts
@@ -1,19 +1,18 @@
-export const toastAndLogError = jest.fn();
+export const toastAndLogError = jest.fn()
 
-const mockLogError = jest.fn();
+const mockLogError = jest.fn()
 
 const logger = {
-  extend: jest.fn(() => ({
-    error: mockLogError,
-    debug: jest.fn(),
-    info: jest.fn(),
-    warn: jest.fn(),
-  })),
-  error: mockLogError,
-  debug: jest.fn(),
-  info: jest.fn(),
-  warn: jest.fn(),
-  MOCK_LOG_ERROR: mockLogError,
-};
+	extend: jest.fn(() => ({
+		error: mockLogError,
+		debug: jest.fn(),
+		info: jest.fn(),
+		warning: jest.fn(),
+	})),
+	error: mockLogError,
+	debug: jest.fn(),
+	info: jest.fn(),
+	warning: jest.fn(),
+}
 
-export default logger;
+export default logger
